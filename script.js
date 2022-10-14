9a3f8ad6c77ef770b91f0787a9e561b595efbc6e
Lance Anderson <lance.a.585@gmail.com>
2022-10-13 20:08:24 -0700

    Fix
    - Fix RegExp so it allows "1(555)555-5555" by adding /\1/ to area code group

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function telephoneCheck(str) {
     let answer;
-    let regExp = /^((1[ -])?(\(\d{3}\)|\d{3})[ -]?(\d{3})[ -]?(\d{4}))$/g;
+    let regExp = /^((1[ -]|(1))?(\(\d{3}\)|\d{3})[ -]?(\d{3})[ -]?(\d{4}))$/g;
   
     answer = regExp.test(str);
   
     return answer;
   }
   
-  console.log(telephoneCheck("(6054756961)"));
+  console.log("(6054756961) - " + telephoneCheck("(6054756961)"));
+  
